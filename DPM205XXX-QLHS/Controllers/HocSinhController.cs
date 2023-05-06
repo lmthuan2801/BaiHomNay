5fcb6bbaa221e722774a1206695cc0ad4e6cd339
LocNgo <ngophuocloc0309@gmail.com>
2023-05-06 21:59:57 +0700

    bai web hom nay

---
+++ DPM205XXX-QLHS/Controllers/HocSinhController.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using DPM205XXX_QLHS.Models;
+using Infrastructure.Entities;
+using Infrastructure.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DPM205XXX_QLHS.Controllers
+{
+    public class HocSinhController : Controller
+    {
+        private readonly IHocSinhService hocSinhService;
+        private readonly IMapper mapper;
+        public HocSinhController(IHocSinhService hocSinhService, IMapper mapper)
+        {
+            this.hocSinhService = hocSinhService;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View(hocSinhService.GetTatCaHocSinh());
+        }
+        
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            HocSinhViewModel data = new HocSinhViewModel();
+            ViewBag.RenderedHtmlTitle = id == 0 ? "THÊM MỚI TÀI KHOẢN" : "CẬP NHẬT TÀI KHOẢN";
+
+            if (id != 0)
+            {
+                HocSinh res = hocSinhService.GetHocSinh(id);
+                data = mapper.Map<HocSinhViewModel>(res);
+                if (data == null)
+                {
+                    return NotFound();
+                }
+            }
+
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddOrEdit(int id, HocSinhViewModel data)
+        {
+            ViewBag.RenderedHtmlTitle = id == 0 ? "THÊM MỚI TÀI KHOẢN" : "CẬP NHẬT TÀI KHOẢN";
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    HocSinh res = mapper.Map<HocSinh>(data);
+                    if (id != 0)
+                    {
+                        hocSinhService.UpdateHocSinh(res);
+                    }
+                    else
+                    {
+
+                        hocSinhService.InsertHocSinh(res);
+                    }
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                }
+
+                return RedirectToAction("Index", "HocSinh");
+            }
+
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            HocSinh res = hocSinhService.GetHocSinh(id);
+            hocSinhService.DeleteHocSinh(res);
+
+            return RedirectToAction("Index", "HocSinh");
+        }
+    }
+}
